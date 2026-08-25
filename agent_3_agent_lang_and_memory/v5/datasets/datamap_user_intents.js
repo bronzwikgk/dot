ad8759d6bcc78a2732763084b98a992a3958a38b
BronzwikGk <67652347+bronzwikgk@users.noreply.github.com>
2026-08-26 02:06:08 +0530

    Add all datamaps: entity relationships, user intents, system operations

---
+++ agent_3_agent_lang_and_memory/v5/datasets/datamap_user_intents.js
@@ -0,0 +1,34 @@
+// datamap_user_intents.js
+// User intents as 1D array
+
+export const datamap_user_intents = [
+  "create_application",
+  "create_website",
+  "create_product_workspace",
+  "create_notebook",
+  "create_book",
+  "edit_cell",
+  "edit_block",
+  "insert_template",
+  "run_workflow",
+  "create_automation",
+  "create_research_agent",
+  "create_team_of_agents",
+  "import_source_document",
+  "compare_and_adopt_learning",
+  "create_fintech_template",
+  "run_trading_backtest",
+  "search_all_entities",
+  "delete_entity",
+  "update_entity",
+  "read_entity",
+  "list_entities",
+  "export_entity",
+  "import_entity",
+  "validate_entity",
+  "reason_about_entity",
+  "compose_response",
+  "display_output",
+  "persist_data",
+  "audit_changes"
+]
